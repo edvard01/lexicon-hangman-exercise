1797f332645f2061c527c4212230c31cbeaa88e5
edvard01 <frejaclueless@gmail.com>
2024-01-31 16:48:55 +0100

    added a json file with a bunch of random words, game now shows user the correct word when you lose. Todo: Be able to win game and give user a choice whether to play again or not

--- js/index.js
+++ js/index.js
@@ -1,57 +1,69 @@
-let counter = 0;
-let wordOfTheDay = "bananmåltid";
-let answer = wordOfTheDay.split("");
-let currentState = wordOfTheDay.split("");
-let gamestate = true;
+getJsonValues().then((wordsArray) => {
+  let counter = 0;
+  let randomNr = Math.floor(Math.random() * wordsArray.length);
+  let wordOfTheDay = wordsArray[randomNr];
+  console.log(wordOfTheDay);
+  let answer = wordOfTheDay.split("");
+  let currentState = wordOfTheDay.split("");
+  let gamestate = true;
+  const svgPaths = document.querySelectorAll("path");
+  const letterBoxes = document.querySelector(".letter-boxes");
 
-const svgPaths = document.querySelectorAll("path");
-const letterBoxes = document.querySelector(".letter-boxes");
+  setup();
 
-setup();
+  const boxes = document.querySelectorAll(".box");
 
-const boxes = document.querySelectorAll(".box");
-
-document.addEventListener("keypress", (e) => {
-  if (gamestate) {
-    let occurences = checkInput(e.key);
-    if (occurences.length === 0) {
-      svgPaths[counter].style.opacity = 1;
-      counter++;
-      if (counter === svgPaths.length) {
-        gamestate = false;
-        gameOver();
+  document.addEventListener("keypress", (e) => {
+    if (gamestate) {
+      let occurences = checkInput(e.key);
+      if (occurences.length === 0) {
+        svgPaths[counter].style.opacity = 1;
+        counter++;
+        if (counter === svgPaths.length) {
+          gamestate = false;
+          gameOver();
+        }
+      } else {
+        occurences.forEach((element) => {
+          boxes[element].childNodes[0].innerHTML =
+            answer[element].toUpperCase();
+        });
       }
-    } else {
-      occurences.forEach((element) => {
-        boxes[element].childNodes[0].innerHTML = answer[element].toUpperCase();
-      });
     }
+  });
+
+  function setup() {
+    svgPaths.forEach((element) => {
+      element.style.opacity = 0;
+    });
+
+    answer.forEach((letter) => {
+      let html = `<div class="box"><h1></h1></div>`;
+      letterBoxes.innerHTML += html;
+    });
+  }
+
+  function checkInput(input) {
+    let occurences = [];
+    for (let index = 0; index < currentState.length; index++) {
+      if (currentState[index] === input) {
+        occurences.push(index);
+        currentState[index] = ".";
+      }
+    }
+    return occurences;
+  }
+
+  function gameOver() {
+    let html = `<h1>GAME OVER <br> Word was: ${wordOfTheDay}</h1>`;
+    letterBoxes.innerHTML = html;
   }
 });
 
-function setup() {
-  svgPaths.forEach((element) => {
-    element.style.opacity = 0;
-  });
-
-  answer.forEach((letter) => {
-    let html = `<div class="box"><h1></h1></div>`;
-    letterBoxes.innerHTML += html;
-  });
-}
-
-function checkInput(input) {
-  let occurences = [];
-  for (let index = 0; index < currentState.length; index++) {
-    if (currentState[index] === input) {
-      occurences.push(index);
-      currentState[index] = ".";
-    }
-  }
-  return occurences;
-}
-
-function gameOver() {
-  let html = "<h1>GAME OVER</h1>";
-  letterBoxes.innerHTML = html;
+function getJsonValues() {
+  return fetch("js/words.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data.words;
+    });
 }
