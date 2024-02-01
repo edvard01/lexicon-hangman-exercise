747895ca5a0714bf7c8db0b1e6d68ac7cde8dfd9
edvard01 <frejaclueless@gmail.com>
2024-02-01 10:48:06 +0100

    Added win screen, added play again button with functionality, changed logic of how the program removes possible letters from the array with letters, this also fixed the issue where user would get a strike if they repeated themselves

--- js/index.js
+++ js/index.js
@@ -22,12 +22,19 @@ getJsonValues().then((wordsArray) => {
         if (counter === svgPaths.length) {
           gamestate = false;
           gameOver();
+          playAgain();
         }
       } else {
         occurences.forEach((element) => {
           boxes[element].childNodes[0].innerHTML =
             answer[element].toUpperCase();
         });
+
+        if (currentState.length === 0) {
+          gamestate = false;
+          win();
+          playAgain();
+        }
       }
     }
   });
@@ -47,8 +54,9 @@ getJsonValues().then((wordsArray) => {
     let occurences = [];
     for (let index = 0; index < currentState.length; index++) {
       if (currentState[index] === input) {
-        occurences.push(index);
-        currentState[index] = ".";
+        occurences = getOccurences(input);
+        currentState.splice(index, 1);
+        break;
       }
     }
     return occurences;
@@ -57,6 +65,33 @@ getJsonValues().then((wordsArray) => {
   function gameOver() {
     let html = `<h1>GAME OVER <br> Word was: ${wordOfTheDay}</h1>`;
     letterBoxes.innerHTML = html;
+  }
+
+  function win() {
+    let newEl = document.createElement("h1");
+    let newText = document.createTextNode("YOU WIN!");
+    newEl.appendChild(newText);
+    letterBoxes.insertAdjacentElement("afterend", newEl);
+  }
+
+  function getOccurences(input) {
+    let occurences = [];
+    for (let index = 0; index < answer.length; index++) {
+      if (answer[index] === input) {
+        occurences.push(index);
+      }
+    }
+    return occurences;
+  }
+
+  const btn = document.querySelector("#play-again-btn");
+  btn.addEventListener("click", (e) => {
+    location.reload();
+  });
+
+  function playAgain() {
+    const btn = document.querySelector("#play-again-btn");
+    btn.style.display = "block";
   }
 });
 
